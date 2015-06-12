353a89698c3dee66f66224d0a1555cfdbf460541
ThalesMR <podsony@hotmail.com>
2015-06-12 17:13:32 -0300

    Listas
    Estou começando a usar lista de espaços livres, mas aina ando aanhando
    na notação e na notação de contrutor.

--- header/memoria.hpp
+++ header/memoria.hpp
@@ -1,14 +1,41 @@
 #ifndef MEMORIA_HPP
 #define MEMORIA_HPP
 
+#include <iostream>
+#include <list>
+
 #define MAX_MEM 1024
 #define MAX_REAL 64
 #define MAX_USUARIO 960
+using namespace std;
+
+struct space{
+    int posicao;
+	int tamanho;
+    struct space *next;
+};
+ 
+typedef struct space Node;
+typedef Node *Ptrnode;
 
 class memoria
 {
 	private:
+		/*atributo estático da Instância da classe para garantir unicidade do objeto*/
+		static memoria *instance = 0;
+		/*Memoria Total*/
 		int mem[MAX_MEM];
+		list<Node> userSpace;
+		list<Node> realSpace;
+		/*Contador de usuário*/
+		int userMem;
+		/*Contador de tempo real*/
+		int realMem;
+		memoria(){}
+		
+	public:
+		static memoria& getInstance();
+		int allocateMem(const int priority, const int quantity);
 };
 
 #endif //MEMORIA_HPP
--- src/memoria.cpp
+++ src/memoria.cpp
@@ -1,3 +1,69 @@
 #include "../header/memoria.hpp"
 
 
+/*Get instance*/
+static memoria& memoria::getInstance()
+{
+	if(!instance) instance = new memoria();
+	return *instance;
+}
+
+void memoria::memoria():userMem (0), realMem(0)
+{
+	/*Inicializa a lista de espaços livres para Usuario*/
+	Node userNo;
+	userNo.posicao = 64;
+	userNo.tam = 960;
+	userNo.next = NULL;
+	this.userSpace.push(userNo);
+	
+	/*Inicializa a lista de espaços livres par tempo real*/
+	Node realNo;
+	realNo.tam = 64;
+	realNo.posicao = 0;
+	realNo.next = NULL;
+	this.realSpace.push(realNo);
+	
+	
+	
+}
+
+
+/*Alocar Memória*/
+/*Retorno é 0 se conseguiu alocar blocos de memória, caso não consiga, retorna 1*/
+/*Entrada é o valor da prioridade que define o tipo de processo e a quantidade de blocos*/
+/*não tratei a questão de ser bloco contiguo de memória ainda.*/
+int memoria::allocateMem(const int priority, const int quantity)
+{
+	if(priority == 0)
+	{
+		if( (this.realMem + quantity) <= MAX_REAL)
+		{
+			/*Caso onde há memória de tempo real livre*/
+			this.realMem += quantity;
+			/*TODO validar espacos contiguos*/
+			
+			return 0;
+		}else
+		{
+			/*Caso de erro onde não foi possível alocar memoria de tempo real*/
+			return 1;
+		}
+	}else
+	{
+		if( (this.userMem + quantity) <= MAX_USUARIO)
+		{
+			/*Caso onde há memória de usuário livre*/
+			this.userMem+=quantity;
+			/*TODO validar espacos contiguos*/
+			
+			return 0;
+		}else
+		{
+			/*Caso de erro onde não foi possível alocar memoria de tempo real*/
+			return 1;
+		}
+	}
+}
+
+
