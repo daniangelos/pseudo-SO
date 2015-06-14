8fc34463c28bf08fc32e4a7ec7e9ed233873c691
ThalesMR <podsony@hotmail.com>
2015-06-14 19:12:02 -0300

    OI
    Concertei uns erros, função de inserir completa.

--- header/memoria.hpp
+++ header/memoria.hpp
@@ -3,37 +3,35 @@
 
 #include <iostream>
 #include <list>
+#include<algorithm>
 
 #define MAX_MEM 1024
 #define MAX_REAL 64
 #define MAX_USUARIO 960
 using namespace std;
 
-struct space{
+struct Space{
     int posicao;
 	int tamanho;
-    struct space *next;
 };
  
-typedef struct space Node;
-typedef Node *Ptrnode;
-
 class memoria
 {
 	private:
 		/*atributo estático da Instância da classe para garantir unicidade do objeto*/
 		static memoria *instance = 0;
 		/*Memoria Total*/
-		int mem[MAX_MEM];
-		list<Node> userSpace;
-		list<Node> realSpace;
+		array<int, MAX_MEM> mem;
+		list<Space> userSpace;
+		list<Space> realSpace;
 		/*Contador de usuário*/
 		int userMem;
 		/*Contador de tempo real*/
 		int realMem;
-		memoria(){}
+		memoria();
 		
 	public:
+		
 		static memoria& getInstance();
 		int allocateMem(const int priority, const int quantity);
 };
--- src/memoria.cpp
+++ src/memoria.cpp
@@ -8,25 +8,25 @@ static memoria& memoria::getInstance()
 	return *instance;
 }
 
-void memoria::memoria():userMem (0), realMem(0)
+memoria::memoria():userMem (0), realMem(0)
 {
 	/*Inicializa a lista de espaços livres para Usuario*/
-	Node userNo;
+	Space userNo;
 	userNo.posicao = 64;
 	userNo.tam = 960;
-	userNo.next = NULL;
-	this.userSpace.push(userNo);
+	this->userSpace.push_back(userNo);
 	
 	/*Inicializa a lista de espaços livres par tempo real*/
-	Node realNo;
+	Space realNo;
 	realNo.tam = 64;
 	realNo.posicao = 0;
-	realNo.next = NULL;
-	this.realSpace.push(realNo);
+	this->realSpace.push_back(realNo);
 	
+	mem.fill(0);
 	
 	
 }
+
 
 
 /*Alocar Memória*/
@@ -34,36 +34,71 @@ void memoria::memoria():userMem (0), realMem(0)
 /*Entrada é o valor da prioridade que define o tipo de processo e a quantidade de blocos*/
 /*não tratei a questão de ser bloco contiguo de memória ainda.*/
 int memoria::allocateMem(const int priority, const int quantity)
-{
+{	
+	auto find_space = [quantity](list<Space>::iterator &begin, list<Space>::iterator &end){
+		return find_if(begin, end, [quantity](Space const &espaco){ return espaco.tamanho >= quantity; });
+	};
+	
 	if(priority == 0)
 	{
 		if( (this.realMem + quantity) <= MAX_REAL)
 		{
 			/*Caso onde há memória de tempo real livre*/
-			this.realMem += quantity;
+			realMem += quantity;
 			/*TODO validar espacos contiguos*/
-			
-			return 0;
-		}else
-		{
-			/*Caso de erro onde não foi possível alocar memoria de tempo real*/
-			return 1;
+			list<Space>::iterator freeSpace = find_space(realSpace.begin(),realSpace.end());
+			if(freeSpace != realSpace.end()) /*achou algo*/
+			{
+				Space usedNo;
+				usedNo.tam = quantity;
+				usedNo.posicao = freeSpace->posicao;
+				this->usedSpace.push_back(usedNo);
+				for(int i=0; i<quantity; i++)
+				{
+					mem[freeSpace->posicacao+i]=1;
+				}
+				if(quantity < freeSpace->tamanho)
+				{
+					freeSpace->tamanho -= quantity;
+					freeSpace->posicao += quantity;
+				}else
+				{
+					realSpace.erase(freeSpace);
+				}
+				return 1;
+			}
 		}
 	}else
 	{
 		if( (this.userMem + quantity) <= MAX_USUARIO)
 		{
 			/*Caso onde há memória de usuário livre*/
-			this.userMem+=quantity;
+			userMem+=quantity;
 			/*TODO validar espacos contiguos*/
-			
-			return 0;
-		}else
-		{
-			/*Caso de erro onde não foi possível alocar memoria de tempo real*/
-			return 1;
+			list<Space>::iterator freeSpace = find_space(userSpace.begin(),userSpace.end());
+			if(freeSpace != userSpace.end()) /*achou algo*/
+			{
+				Space usedNo;
+				usedNo.tam = quantity;
+				usedNo.posicao = freeSpace->posicao;
+				this->usedSpace.push_back(usedNo);
+				for(int i=0; i<quantity; i++)
+				{
+					mem[freeSpace->posicacao+i]=1;
+				}
+				if(quantity < freeSpace->tamanho)
+				{
+					freeSpace->tamanho -= quantity;
+					freeSpace->posicao += quantity;
+				}else
+				{
+					userSpace.erase(freeSpace);
+				}
+				return 1;
+			}
 		}
 	}
+	return 0;
 }
 
 
